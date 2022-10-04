b0b8844022d3947db93a56dc4a68322fff754386
penguin234 <56291718+penguin234@users.noreply.github.com>
2022-10-04 22:54:04 +0900

    Initial setting

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+const dbpool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+        ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log('listening on *:' + PORT.toString());
+});
